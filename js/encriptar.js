cc406ea6d398fb61c707cebf5b687222a40dfb5e
druiz18 <d.ruiz2257@gmail.com>
2023-01-14 15:15:39 -0500

    Correccion de enlaces4

--- js/encriptar.js
+++ js/encriptar.js
@@ -2,7 +2,7 @@ function encriptar() {
     // Obtener el mensaje del textarea
     var mensaje = document.getElementById("mensaje").value;
     // Validar que el mensaje esté escrito en minúsculas y no tenga tildes
-    if (/[^a-z ]/.test(mensaje)) {
+    if (/[^a-zñ ]/.test(mensaje)) {
         alert("El mensaje debe estar escrito en minúsculas y no debe tener tildes");
         return;
     }
@@ -15,8 +15,8 @@ function encriptar() {
     // Mostrar el mensaje encriptado en el textarea
     document.getElementById("resultado").value = mensaje;
 }
-var btn = document.getElementById("encriptar");
-btn.onclick = encriptar();
+// var btn = document.getElementById("encriptar");
+// btn.onclick = encriptar();
 
 function desencriptar() {
     // Obtener el mensaje del textarea
@@ -30,5 +30,5 @@ function desencriptar() {
     // Mostrar el mensaje desencriptado en el textarea
     document.getElementById("resultado").value = mensaje;
 }
-var btn = document.getElementById("desencriptar");
-btn.onclick = desencriptar();
+// var btn = document.getElementById("desencriptar");
+// btn.onclick = desencriptar();
